5a52de88a8703d16be1034b1ca1095a1b31e9884
FelipeHJBastos <felipe.123.bastos@gmail.com>
2024-02-27 21:58:21 -0300

    fix: ajuste em listen de post e host

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
 import { Application, oakCors, config } from "../deps.ts";
 import router from "./router.ts";
 
+const host = Deno.env.get("APP_HOST");
+const port = Deno.env.get("APP_PORT");
+
 // Necessário para carregar as variáveis de ambiente
 await config({export: true});
 
 const app = new Application();
 
-
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Server running on http://${ Deno.env.get("APP_HOST")}:${Deno.env.get("APP_PORT")}/`);
+console.log(`Server running on http://${host}:${port}/`);
 
-const options = {
-    hostname: Deno.env.get("APP_HOST" ?? "localhost"),
-    port: Number(Deno.env.get("APP_PORT" ?? "")),
-}
-
-await app.listen(options);
+await app.listen(`${host}:${port}`);
