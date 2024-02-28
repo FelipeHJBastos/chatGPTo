7cc226bc3b709e25b973507d073953446e309c99
FelipeHJBastos <felipe.123.bastos@gmail.com>
2024-02-27 22:00:01 -0300

    fix: reajuste

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Application, oakCors, config } from "../deps.ts";
 import router from "./router.ts";
 
-const host = Deno.env.get("APP_HOST");
-const port = Deno.env.get("APP_PORT");
+const host = Deno.env.get("APP_HOST") || "localhost";
+const port = Deno.env.get("APP_PORT") || 8000;
 
 // Necessário para carregar as variáveis de ambiente
 await config({export: true});
