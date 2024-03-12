be0a5a1c3fdb1b507000374efbfac48e3bf6f061
felipe.hariel <felipe.hariel@PC-286>
2024-03-12 09:10:05 -0300

    teste1

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -1,10 +1,4 @@
 import { GetUser } from "../Repositories/userRepository.ts";
-
-/**
- * Returns the user obtained from the GetUser function.
- *
- * @return {type} The user object
- */
 export const ReturnUser = () => {
     const user = GetUser();
     return user;
